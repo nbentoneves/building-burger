9196d7371b38ee54b6459ca885d013e2927d0645
nbentoneves <nbento.neves@gmail.com>
2020-09-30 15:29:42 +0100

    Updates

---
+++ precache-manifest.18c41c4a391e2f4c542fa9f8a8623cad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f490504bf41bb2e5c7871e81adec65",
+    "url": "/building-burger/index.html"
+  },
+  {
+    "revision": "fa484545a4d141ec61f5",
+    "url": "/building-burger/static/css/main.6c24c382.chunk.css"
+  },
+  {
+    "revision": "2b77fb9fca906e6d9b8b",
+    "url": "/building-burger/static/js/2.5fc1b3f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/building-burger/static/js/2.5fc1b3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa484545a4d141ec61f5",
+    "url": "/building-burger/static/js/main.74537e7d.chunk.js"
+  },
+  {
+    "revision": "ff9c7021ee0026bd4408",
+    "url": "/building-burger/static/js/runtime-main.27ad5cdb.js"
+  }
+]);
